9d92c642d1852c444bbc6b4e568ed0bf2e30a57a
whiplashz <pilvorm@gmail.com>
2021-11-01 22:54:01 +0100

    Add function for fast reading of integers

--- algorithms/4-caravan-in-the-desert/main.cpp
+++ algorithms/4-caravan-in-the-desert/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int INT_MAX_LEN = 10;
+
+/**
+ * Read one non-negative integer from standard input, ignoring any non-digit characters.
+ *
+ * @return integer if one was in input, EOF otherwise.
+ */
+int get_integer_from_stdin() {
+    unsigned char str[INT_MAX_LEN];
+    int c = 0, i = 0;
+
+    while ((c = getchar()) != EOF && !isdigit(c));
+    while (c != EOF && isdigit(c)) {
+        str[i++] = (unsigned char)c;
+        c = getchar();
+    }
+
+    int num = 0, m = 1;
+    while (i--) {
+        num += (str[i] - '0') * m;
+        m *= 10;
+    }
+
+    return m != 1 ? num : EOF;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
