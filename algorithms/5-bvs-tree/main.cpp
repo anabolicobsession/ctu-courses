d1815e5d566fbcef909b8d258c1d6c60d6628b7e
whiplashz <pilvorm@gmail.com>
2021-11-06 13:09:01 +0100

    Initial commit

---
+++ algorithms/5-bvs-tree/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "utils.h"
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+
+    return 0;
+}
---
+++ algorithms/5-bvs-tree/utils.h
@@ -0,0 +1,12 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+/**
+ * Read one non-negative integer from standard input, ignoring any non-digit characters.
+ * Useful for fast reading.
+ *
+ * @return integer if one was in input, EOF otherwise.
+ */
+int get_integer_from_stdin();
+
+#endif //UTILS_H_
