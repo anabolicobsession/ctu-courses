f03edebde0b40645cd6eb0071d4659f90b79a01b
whiplashz <pilvorm@gmail.com>
2021-11-21 00:25:42 +0100

    Rename height to move

--- algorithms/7-retro-game/main.cpp
+++ algorithms/7-retro-game/main.cpp
@@ -7,10 +7,10 @@ using namespace std;
 class RetroGame {
 private:
     vector<vector<int>> eggs;
-    int playing_area_width, max_egg_height, n_eggs;
+    int playing_area_width, n_moves, n_eggs;
 
 public:
-    RetroGame() : playing_area_width(0), max_egg_height(0), n_eggs(0) {}
+    RetroGame() : playing_area_width(0), n_moves(0), n_eggs(0) {}
 
     void initialize_from_stdin() {
         cin >> playing_area_width >> n_eggs;
@@ -18,16 +18,16 @@ public:
         auto tmp_eggs = vector<vector<int>>(1 + playing_area_width, vector<int>());
 
         for (int i = 0; i < n_eggs; ++i) {
-            int egg_column, egg_height;
-            cin >> egg_column >> egg_height;
-            tmp_eggs[egg_column].push_back(egg_height);
+            int egg_position, egg_move;
+            cin >> egg_position >> egg_move;
+            tmp_eggs[egg_position].push_back(egg_move);
 
-            if (egg_height > max_egg_height) {
-                max_egg_height = egg_height;
+            if (egg_move > n_moves) {
+                n_moves = egg_move;
             }
         }
 
-        eggs = vector<vector<int>>(1 + max_egg_height, vector<int>());
+        eggs = vector<vector<int>>(1 + n_moves, vector<int>());
         for (int i = 1; i <= playing_area_width; ++i) {
             for (const auto &egg_height: tmp_eggs[i]) {
                 eggs[egg_height].push_back(i);
@@ -41,17 +41,17 @@ public:
         vector<int> best_score(1 + playing_area_width, UNDEFINED), new_best_score(1 + playing_area_width, UNDEFINED);
         best_score[1] = 0;
 
-        for (int height = 1; height <= max_egg_height; ++height) {
-            vector<int> n_eggs_at_position(1 + playing_area_width, 0);
-            for (const auto &egg_col: eggs[height]) {
-                n_eggs_at_position[egg_col - 1]++;
-                n_eggs_at_position[egg_col]++;
+        for (int move = 1; move <= n_moves; ++move) {
+            vector<int> n_eggs_at_move(1 + playing_area_width, 0);
+            for (const auto &egg_pos: eggs[move]) {
+                n_eggs_at_move[egg_pos - 1]++;
+                n_eggs_at_move[egg_pos]++;
             }
 
             for (int pos = 1; pos <= N_BASKET_POSITIONS; ++pos) {
                 new_best_score[pos] = max({best_score[pos - 1], best_score[pos], best_score[pos + 1]});
                 if (new_best_score[pos] != UNDEFINED) {
-                    new_best_score[pos] += n_eggs_at_position[pos];
+                    new_best_score[pos] += n_eggs_at_move[pos];
                 }
             }
 
