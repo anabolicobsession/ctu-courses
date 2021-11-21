39ef5509ab54285d5febc99f5bf26c500c6f5cc0
whiplashz <pilvorm@gmail.com>
2021-11-22 00:57:29 +0100

    Add TotemVillages class

--- algorithms/8-totems-in-the-african-rainforest/main.cpp
+++ algorithms/8-totems-in-the-african-rainforest/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+class TotemVillages {
+private:
+    vector<int> fighters;
+    int n_villages;
+    int totem_price;
+    int fighter_price;
+
+public:
+    TotemVillages() : n_villages(0), totem_price(0), fighter_price(0) {}
+
+    void initialize_from_stdin() {
+        cin >> n_villages >> totem_price >> fighter_price;
+        fighters.resize(n_villages);
+        for (int i = 0; i < n_villages; ++i) {
+            cin >> fighters[i];
+        }
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    TotemVillages tvs;
+    tvs.initialize_from_stdin();
+
     return 0;
 }
