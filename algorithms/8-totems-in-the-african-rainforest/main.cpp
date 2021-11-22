2f76be2cf255115a18521472f548e5535f114001
whiplashz <pilvorm@gmail.com>
2021-11-22 21:03:13 +0100

    Add auxiliary matrix for sum of fighters

--- algorithms/8-totems-in-the-african-rainforest/main.cpp
+++ algorithms/8-totems-in-the-african-rainforest/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <iomanip>
 #include <limits>
-#include <numeric>
-#include <algorithm>
 #include <vector>
 
 using namespace std;
@@ -56,34 +54,32 @@ public:
 
     int compute_max_profit_from_purchase_of_totems() const {
         vector<vector<int>> profit_matrix(n_villages, vector<int>(n_villages, numeric_limits<int>::min()));
+        vector<vector<int>> fighters_sum(n_villages, vector<int>(n_villages, 0));
 
         for (int i = 0; i < n_villages; ++i) {
             profit_matrix[i][i] = 0;
+            fighters_sum[i][i] = fighters[i];
         }
+        auto max_profit_matrix = profit_matrix;
 
         for (int len = 2; len <= n_villages; ++len) {
             for (int start = 0, end = start + len - 1; end < n_villages; ++start, ++end) {
+                fighters_sum[start][end] = fighters_sum[start][end - 1] + fighters_sum[end][end];
+
                 for (int totem = start; totem < end; ++totem) {
-                    int diff_in_fighters = abs(accumulate(fighters.begin() + start, fighters.begin() + totem + 1, 0) -
-                                               accumulate(fighters.begin() + totem + 1, fighters.begin() + end + 1, 0));
+                    int diff_in_fighters = abs(fighters_sum[start][totem] - fighters_sum[totem + 1][end]);
                     int profit = totem_price - fighter_price * diff_in_fighters + profit_matrix[start][totem] + profit_matrix[totem + 1][end];
                     profit_matrix[start][end] = max(profit, profit_matrix[start][end]);
+
+                    int left_max_profit = max_profit_matrix[start][totem];
+                    int right_max_profit = max_profit_matrix[totem + 1][end];
+                    int max_profit = max(left_max_profit + right_max_profit, max(left_max_profit, right_max_profit));
+                    max_profit_matrix[start][end] = max(max_profit, max(profit_matrix[start][end], max_profit_matrix[start][end]));
                 }
             }
         }
 
-        for (int len = 2; len <= n_villages; ++len) {
-            for (int start = 0, end = start + len - 1; end < n_villages; ++start, ++end) {
-                for (int totem = start; totem < end; ++totem) {
-                    profit_matrix[start][end] = max({profit_matrix[start][totem] + profit_matrix[totem + 1][end],
-                                                     profit_matrix[start][totem],
-                                                     profit_matrix[totem + 1][end],
-                                                     profit_matrix[start][end]});
-                }
-            }
-        }
-
-        return profit_matrix[0][n_villages - 1];
+        return max_profit_matrix[0][n_villages - 1];
     }
 };
 
